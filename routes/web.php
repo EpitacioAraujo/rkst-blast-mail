847debd8426953de9d8f52dd3e40f213ec00c230
epitacio araujo <epitacio.sobrinho00@hotmail.com>
2025-07-04 14:24:07 -0300

    Restore welcome route for guests while maintaining dashboard for authenticated users

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,6 @@ use App\Http\Controllers\EmailListController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['guest'])->group(function () {
-    Route::get('/', function () {
-        return view('welcome');
-    });
-});
-
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('/', 'dashboard')->name('dashboard');
 
@@ -19,6 +13,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('/email-lists', [EmailListController::class, 'index'])->name('email_lists.index');
     Route::get('/email-lists/create', [EmailListController::class, 'create'])->name('email_lists.create');
+});
+
+Route::get('/', function () {
+    return view('welcome');
 });
 
 require __DIR__.'/auth.php';
